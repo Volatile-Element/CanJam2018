212537004f4076190b9e794df4bd1205210c466d
Jordan Bird <jordanbird-29@hotmail.co.uk>
2018-03-03 19:57:39 +0000

    Add Navigation, Whitebox and View in Range

---
+++ Assets/Scripts/Managers/WorldManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : Singleton<WorldManager>
+{
+    public float Width = 100;
+    public float Height = 100;
+}
